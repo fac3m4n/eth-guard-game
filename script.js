09e01ded0cf377acfeeb6cefdbbe4fc27826c5f8
fac3m4n <akerimberdi@gmail.com>
2024-08-09 22:35:48 +0300

    remove off screen projectiles

--- script.js
+++ script.js
@@ -107,9 +107,23 @@ function animate() {
   animationId = requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.draw();
-  projectiles.forEach((projectile) => {
+
+  projectiles.forEach((projectile, index) => {
     projectile.update();
+
+    // remove projectile if it goes off the screen
+    if (
+      projectile.x - projectile.radius < 0 ||
+      projectile.x - projectile.radius > canvas.width ||
+      projectile.y + projectile.radius < 0 ||
+      projectile.y - projectile.radius > canvas.height
+    ) {
+      setTimeout(() => {
+        projectiles.splice(index, 1);
+      }, 0);
+    }
   });
+
   enemies.forEach((enemy, index) => {
     enemy.update();
     const dist = Math.hypot(player.x - enemy.x, player.y - enemy.y);
