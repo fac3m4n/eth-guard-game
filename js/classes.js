2990048c91a762078639701f7fc2c43b1533612b
fac3m4n <akerimberdi@gmail.com>
2024-08-10 02:06:01 +0300

    adding dynamic bg effect

--- js/classes.js
+++ js/classes.js
@@ -174,6 +174,25 @@ class Particle {
   }
 }
 
+class BackgroundParticle {
+  constructor({ position, radius = 3, color = "blue" }) {
+    this.position = position;
+    this.radius = radius;
+    this.color = color;
+    this.alpha = 0.1;
+  }
+
+  draw() {
+    c.save();
+    c.globalAlpha = this.alpha;
+    c.beginPath();
+    c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+    c.fillStyle = this.color;
+    c.fill();
+    c.restore();
+  }
+}
+
 class PowerUp {
   constructor({ position = { x: 0, y: 0 }, velocity }) {
     this.position = position;
--- script.js
+++ script.js
@@ -23,6 +23,7 @@ let intervalId;
 let score = 0;
 let powerUps = [];
 let frames = 0;
+let backgroundParticles = [];
 
 function init() {
   player = new Player(x, y, 10, "white");
@@ -34,6 +35,23 @@ function init() {
   score = 0;
   playerScore.innerHTML = 0;
   frames = 0;
+  backgroundParticles = [];
+
+  const spacing = 30;
+
+  for (let x = 0; x < canvas.width + spacing; x += spacing) {
+    for (let y = 0; y < canvas.height + spacing; y += spacing) {
+      backgroundParticles.push(
+        new BackgroundParticle({
+          position: {
+            x,
+            y,
+          },
+          radius: 3,
+        })
+      );
+    }
+  }
 }
 
 function spawnEnemies() {
@@ -106,6 +124,27 @@ function animate() {
   c.fillStyle = "rgba(30, 30, 30, 0.1)";
   c.fillRect(0, 0, canvas.width, canvas.height);
   frames++;
+
+  backgroundParticles.forEach((backgroundParticle) => {
+    backgroundParticle.draw();
+
+    const dist = Math.hypot(
+      player.x - backgroundParticle.position.x,
+      player.y - backgroundParticle.position.y
+    );
+
+    if (dist < 100) {
+      backgroundParticle.alpha = 0;
+
+      if (dist > 70) {
+        backgroundParticle.alpha = 0.5;
+      }
+    } else if (dist > 100 && backgroundParticle.alpha < 0.1) {
+      backgroundParticle.alpha += 0.01;
+    } else if (dist > 100 && backgroundParticle.alpha > 0.1) {
+      backgroundParticle.alpha -= 0.01;
+    }
+  });
 
   player.update();
 
@@ -266,6 +305,20 @@ function animate() {
             },
             score: 150,
           });
+
+          // change background particle colors
+          backgroundParticles.forEach((backgroundParticle) => {
+            gsap.set(backgroundParticle, {
+              color: "white",
+              alpha: 1,
+            });
+            gsap.to(backgroundParticle, {
+              color: enemy.color,
+              alpha: 0.1,
+            });
+            // backgroundParticle.color = enemy.color
+          });
+
           enemies.splice(index, 1);
           projectiles.splice(projectilesIndex, 1);
         }
